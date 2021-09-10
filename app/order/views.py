d6ed0a92b5af1bc00afef70bddba2a79466ee556
amitbhalla <amit9815@gmail.com>
2021-09-10 19:28:59 +0530

    Added test API

---
+++ app/order/views.py
@@ -0,0 +1,11 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+@api_view(["GET"])
+def test_view(request):
+    response = {
+        "message": "Order API is working.",
+        "URL": request.get_full_path(),
+    }
+    return Response(response)
--- app/proj/urls.py
+++ app/proj/urls.py
@@ -1,9 +1,36 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path(
+        "admin/",
+        admin.site.urls,
+    ),
+    path(
+        "api/chapters/",
+        include("chapter.urls"),
+    ),
+    path(
+        "api/coupons/",
+        include("coupon.urls"),
+    ),
+    path(
+        "api/courses/",
+        include("course.urls"),
+    ),
+    path(
+        "api/doubts/",
+        include("doubt.urls"),
+    ),
+    path(
+        "api/orders/",
+        include("order.urls"),
+    ),
+    path(
+        "api/reviews/",
+        include("review.urls"),
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
