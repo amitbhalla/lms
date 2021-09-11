84e26f13974ff3430446f1590df44a020953768d
amitbhalla <amit9815@gmail.com>
2021-09-11 15:08:11 +0530

    Changed to Generics

--- app/course/urls.py
+++ app/course/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 
-from .views import CategoryListView
+from .views import CategoryListView, CategoryDetailView
 
 urlpatterns = [
+    path(
+        "categories/<str:pk>/",
+        CategoryDetailView.as_view(),
+        name="categories-detailview",
+    ),
     path(
         "categories/",
         CategoryListView.as_view(),
--- app/course/views.py
+++ app/course/views.py
@@ -1,25 +1,17 @@
-from rest_framework import views
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.parsers import JSONParser
+from rest_framework.generics import (
+    ListCreateAPIView,
+    RetrieveUpdateDestroyAPIView,
+)
 
 from .models import Category
 from .serializers import CategorySerializer
 
 
-class CategoryListView(views.APIView):
-    def get(self, request):
-        categories = Category.objects.all()
-        serializer = CategorySerializer(categories, many=True)
-        return Response(serializer.data)
+class CategoryListView(ListCreateAPIView):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all().order_by("-created_date")
 
-    def post(self, request):
-        json = JSONParser().parse(request)
-        serializer = CategorySerializer(data=json)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        else:
-            return Response(
-                serializer.errors, status=status.HTTP_400_BAD_REQUEST
-            )
+
+class CategoryDetailView(RetrieveUpdateDestroyAPIView):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
