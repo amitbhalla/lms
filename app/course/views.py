e1e227fbc4cadb363042a2e96a4befabd0e83821
amitbhalla <amit9815@gmail.com>
2021-09-11 12:30:46 +0530

    Added Post Serializer

--- app/course/views.py
+++ app/course/views.py
@@ -1,5 +1,7 @@
 from rest_framework import views
 from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.parsers import JSONParser
 
 from .models import Category
 from .serializers import CategorySerializer
@@ -10,3 +12,14 @@ class CategoryListView(views.APIView):
         categories = Category.objects.all()
         serializer = CategorySerializer(categories, many=True)
         return Response(serializer.data)
+
+    def post(self, request):
+        json = JSONParser().parse(request)
+        serializer = CategorySerializer(data=json)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(
+                serializer.errors, status=status.HTTP_400_BAD_REQUEST
+            )
